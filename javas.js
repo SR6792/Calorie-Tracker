27910a51ad1d9263c4f674ee868c3dcc5ab560fe
SR6792 <sooraj.2050in@gmail.com>
2025-08-06 16:35:47 +0000

    made the strucutre

---
+++ javas.js
@@ -0,0 +1,9 @@
+async function getCal(){
+    try{
+        const foodName=inp.value.toLower().trim();
+        const response = await fetch(``)
+    }
+    catch(err){
+        console.error(err);
+    }
+}
